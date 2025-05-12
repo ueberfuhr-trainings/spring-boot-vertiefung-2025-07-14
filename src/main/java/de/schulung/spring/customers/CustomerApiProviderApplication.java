f89e816940b1d4dbcd61e3dae8173ea464b05efd
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2025-05-12 11:07:06 +0200

    Prepare project for coding.

--- src/main/java/de/schulung/spring/customers/CustomerApiProviderApplication.java
+++ src/main/java/de/schulung/spring/customers/CustomerApiProviderApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CustomerApiProviderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerApiProviderApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CustomerApiProviderApplication.class, args);
+  }
 
 }
