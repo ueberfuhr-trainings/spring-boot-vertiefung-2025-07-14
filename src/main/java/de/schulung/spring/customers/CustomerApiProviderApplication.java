728ee14e28252e8b8d79919a69ecd148d4a7f46a
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2025-05-12 11:00:06 +0200

    Initial commit

---
+++ src/main/java/de/schulung/spring/customers/CustomerApiProviderApplication.java
@@ -0,0 +1,13 @@
+package de.schulung.spring.customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApiProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApiProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/schulung/spring/customers/CustomerApiProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package de.schulung.spring.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApiProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
